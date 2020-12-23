0446d91c85ec11f5bf67fb479edb6e3d951eb970
kamath-abhijith <kamath.abhijith@gmail.com>
2020-12-24 04:13:26 +0530

    clipping algorithm for TV

--- denoising/main_TV.py
+++ denoising/main_TV.py
@@ -21,13 +21,24 @@ from sklearn.feature_extraction.image import reconstruct_from_patches_2d
 
 # %% FUNCTION DEFINITION
 
+# CLIPPING FUNCTION
+def clip(b, T):
+    out = np.zeros(len(b))
+    for i in range(len(b)):
+        if np.abs(b[i]) <= T:
+            out[i] = b[i]
+        else:
+            out[i] = T*np.sign(b[i])
+    
+    return out
+
 # QUADRATIC SMOOTHING
 def quad_smoothing(x, delta=1):
-    ''' INPUT: Data vector, x
+    ''' INPUT: Measurement, x
                Denoising weight, delta
     
         OUTPUT: Quadratic smoothing
-                x^* = arg min || z - x ||^2 + delta || Dz ||^2,
+                z^* = arg min || z - x ||^2 + delta || Dz ||^2,
                 where D is the first difference operator
     '''
     
@@ -38,29 +49,56 @@ def quad_smoothing(x, delta=1):
     return np.linalg.solve(A,x)
 
 # TOTAL VARIATION DENOISING
-def TV_denoising(x, delta=1):
-    return
+def clipping_TV(y, lambd=1, alpha=1, tol=1e-6, max_iterations=25):
+    ''' INPUT: Measurement, y
+               Denoising weight, lambda
+               Majoriser weight, alpha
+    
+        OUTPUT: Total Variation Denoising
+                z^* = arg min || z - x ||^2 + lambd || Dz ||^1,
+                where D is the first difference operator
+    '''
+
+    N = len(y)
+    D = sp.linalg.toeplitz(np.hstack(([-1],np.zeros(N-2))),np.hstack(([-1,1],np.zeros(N-2))))
+
+    z = np.zeros(N-1)
+    for _ in range(max_iterations):
+        zcopy = z
+
+        x = y - (D.T).dot(z)
+        z = clip(z+D.dot(x)/alpha, lambd/2)
+        if np.linalg.norm(z-zcopy)<=tol:
+            break
+
+    return x
 
 
 # %% MAIN
-image = io.imread('images/lena.tif', 0)
+image = io.imread('images/house.tif', 0)
 m,n = image.shape
 
-patch_size = (16,16)
-patches = extract_patches_2d(image, patch_size)
+sigma = 10
+noisy_image = image + sigma*np.random.randn(m,n)
 
-delta = 1
+patch_size = (8,8)
+patches = extract_patches_2d(noisy_image, patch_size)
+
+lambd = 10
+alpha = 2
 processed_patches = np.zeros(patches.shape)
 for i in range(patches.shape[0]):
-    smooth_patch = quad_smoothing(patches[i].flatten(), delta)
+    # smooth_patch = quad_smoothing(patches[i].flatten(), delta)
+    smooth_patch = clipping_TV(patches[i].flatten(), lambd, alpha)
     processed_patches[i] = smooth_patch.reshape(patch_size)
 
 smooth_image = reconstruct_from_patches_2d(processed_patches, image.shape)
 
 # %% PLOTS: QUADRATIC SMOOTHING
-fig, plts = plt.subplots(1,2, figsize=(10,6))
+fig, plts = plt.subplots(1,3, figsize=(15,6))
 plts[0].imshow(image, cmap='gray')
-plts[1].imshow(smooth_image, cmap='gray')
+plts[1].imshow(noisy_image, cmap='gray')
+plts[2].imshow(smooth_image, cmap='gray')
 plt.show()
 
 # %%
